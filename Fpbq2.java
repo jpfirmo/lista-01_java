0b525b4a44c0436756f913b60b8da4e4b5a7c414
Jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-04-04 15:58:58 -0300

    Add files via upload

---
+++ Fpbq2.java
@@ -0,0 +1,26 @@
+package teste01java;
+
+import java.util.Scanner;
+
+public class Fpbq2 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada =  new Scanner (System.in);
+		System.out.println("digite o valor de fahrenheit:");
+		double fahrenheit = entrada.nextDouble();
+		
+		double celsius = (5*(fahrenheit - 32)/9);
+		System.out.println("valor em celsius:" + celsius);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
