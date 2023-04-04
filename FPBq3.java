8aed09e40db48417a926a94973c1b8d7a6f83e57
Jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-04-04 16:00:33 -0300

    Add files via upload

---
+++ FPBq3.java
@@ -0,0 +1,28 @@
+package teste01java;
+
+import java.util.Scanner;
+
+public class FPBq3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("digite o valor de celsius:");
+		double celsius = entrada.nextDouble();
+		double F = (celsius * 1.8)+32;
+		System.out.println("valor de fahrenheit:" + F);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
