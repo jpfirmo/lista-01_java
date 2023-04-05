d8ae05e678a1190ee3375b348f85ff9baeb4a26e
Jpfirmo <127751417+jpfirmo@users.noreply.github.com>
2023-04-05 10:19:04 -0300

    Add files via upload

---
+++ FPB_q6.java
@@ -0,0 +1,28 @@
+package teste01java;
+
+import java.util.Scanner;
+
+public class FPB_q6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("insira um número:");
+		int número = entrada.nextInt();
+		int sucessor = número + 1;
+		int antecessor = número - 1;
+		System.out.println("o sucessor é:" + sucessor);
+		System.out.println("o antecessor é:" + antecessor);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
